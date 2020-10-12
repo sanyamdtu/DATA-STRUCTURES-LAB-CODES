7d796585b520f65e8f7f83e891ca2e3363802636
Sanyam Srivastava <you@example.com>
2020-10-12 23:54:29 +0530

    hi

--- linkedlistheadnode.cpp
+++ linkedlistheadnode.cpp
@@ -1,141 +1,178 @@
-#include <iostream>
 #include <stdio.h>
-using namespace std;
-
-struct node
+#include <stdlib.h>
+struct linked
 {
     int val;
-    struct node *next;
+    struct linked *next;
 };
-struct head
+struct h
 {
-    struct node *start;
-    struct node *end;
+    struct linked *p;
+    struct linked *end;
 };
-void InsertAtEnd(struct head *hd, int x)
+void InsertAtEnd(struct h *hd, int x)
 {
-    struct node *temp;
-    temp = (struct node *)malloc(100 * sizeof(struct node));
+    struct linked *temp;
+    temp = (struct linked *)malloc(sizeof(struct linked));
     temp->val = x;
     temp->next = NULL;
-    if (hd->start == NULL)
+    if (hd->p == NULL)
     {
-        hd->start = temp;
+        hd->p = temp;
         hd->end = temp;
     }
     else
         hd->end->next = temp;
 }
-void InsertAtFront(struct head *hd, int x)
+void particular_insert_after(struct h *hd, int x)
 {
-    struct node *temp;
-    temp = (struct node *)malloc(sizeof(struct node));
+    struct linked *temp;
+    temp = (struct linked *)malloc(sizeof(struct linked));
     temp->val = x;
     temp->next = NULL;
-    if (hd->start == NULL)
+    if (hd->p == NULL)
     {
-        hd->start = temp;
+        hd->p = temp;
         hd->end = temp;
     }
     else
     {
-        temp->next = hd->start;
-        hd->start = temp;
+        temp->next = hd->p;
+        hd->p = temp;
     }
 }
-void InsertAfter(struct head *hd, int x, int a)
+void InsertAfter(struct h *hd, int x, int a)
 {
-    struct node *temp;
-    temp = (struct node *)malloc(sizeof(struct node));
+    struct linked *temp;
+    temp = (struct linked *)malloc(sizeof(struct linked));
     temp->val = x;
     temp->next = NULL;
-    struct node *h = hd->start;
+    struct linked *h = hd->p;
     while (h->val != a)
         h = h->next;
     temp->next = h->next;
     h->next = temp;
 }
-int search(struct head h, int x)
+int search(struct h *h, int x)
 {
-    while (h.start != NULL)
+    while (h->p != NULL)
     {
-        if (h.start->val == x)
+        if (h->p->val == x)
             return 1;
-        h.start = h.start->next;
+        h->p = h->p->next;
     }
     return 0;
 }
-void deletell(struct head *hd, int x)
+void dellinked_list(struct h *hd, int x)
 {
-    struct node *t = hd->start;
+    if (hd->p == NULL)
+    {
+        printf("underflow empty \n");
+        return;
+    }
+    struct linked *t = hd->p;
     if (t->val == x)
-        hd->start = t->next;
+        hd->p = t->next;
     else
     {
-        struct node *p = t;
+        struct linked *p = t;
         t = t->next;
-        while (t->val != x)
+        while (t && t->val != x)
         {
             t = t->next;
             p = p->next;
         }
+        if (t == NULL)
+        {
+            printf("not deleted not found\n");
+            return;
+        }
         p->next = t->next;
+        printf("deleted \n");
         free(t);
     }
 }
-int count(struct head h)
+int total(struct h h)
 {
     int ans = 0;
-    while (h.start != NULL)
+    while (h.p != NULL)
     {
-        h.start = h.start->next;
+        h.p = h.p->next;
         ans++;
     }
     return ans;
 }
+void display(struct linked *s)
+{
+    struct linked *p = s;
+    if (p == NULL)
+    {
+        printf("Linked list is empty.");
+        return;
+    }
+    while (p->next != NULL)
+    {
+        printf("%d-> ", p->val);
+        p = p->next;
+    }
+    printf("%d", p->val);
+}
 int main()
 {
-    struct head hd;
-    hd.start = hd.end = NULL;
+    struct h hd;
+    hd.p = hd.end = NULL;
     int c, x, f;
     while (1)
     {
-        printf("1. Insert at end\n2. Insert at front\n3. Insert after\n4.Search\n5. Delete\n6. Count\n7. Exit\n");
+        printf("\n1. Insert at end\n2. Insert at front\n3. Insert after\n4. Search\n5. Delete\n6. total\n7. Display\n8. Exit");
+        printf("\nEnter the choice : ");
         scanf("%d", &c);
         if (c == 1)
         {
+            printf("\nEnter the number to be inserted at end.");
             scanf("%d", &x);
             InsertAtEnd(&hd, x);
         }
         if (c == 2)
         {
+            printf("\nEnter the number to be inserted at front.");
             scanf("%d", &x);
-            InsertAtFront(&hd, x);
+            particular_insert_after(&hd, x);
         }
         if (c == 3)
         {
-            scanf("%d%d", &x, &f);
+            printf("\nEnter the number to after which you want to insert.");
+            scanf("%d", &f);
+            printf("\nEnter the number to be inserted.");
+            scanf("%d", &x);
             InsertAfter(&hd, x, f);
         }
         if (c == 4)
         {
+            printf("\nEnter the number to be searched.");
             scanf("%d", &x);
-            if (search(hd, x) > 0)
+            if (search(&hd, x) > 0)
                 printf("Found\n");
             else
                 printf("Not Found\n");
         }
         if (c == 5)
         {
+            printf("\nEnter the number to be deleted.");
             scanf("%d", &x);
-            deletell(&hd, x);
+            dellinked_list(&hd, x);
         }
         if (c == 6)
         {
-            x = count(hd);
-            printf("%d\n", x);
+            x = total(hd);
+            printf("\nThe number of elements present are : %d", x);
         }
         if (c == 7)
+        {
+            struct linked *p = hd.p;
+            display(p);
+        }
+        if (c == 8)
             return 0;
     }
 }
